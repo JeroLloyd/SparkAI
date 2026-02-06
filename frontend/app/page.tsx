3660a83dcded8f1c0f2eba37f0982c43510e4836
MagnoJl <22-04009@g.batstate-u.edu.ph>
2026-02-07 02:51:27 +0800

    up

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useRef, useEffect } from "react";
-import ReactMarkdown, { Components } from "react-markdown";
+import ReactMarkdown, { type Components } from "react-markdown";
 
 // --- TYPES ---
 interface Message {
